fc7852ae9b74d1e7df7c90924cde3d3600d5fb64
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-02-07 00:23:37 -0300

    Se agregaron componentes a page-sin estilos-

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import Contact from "./components/Contact";
+import Skill from "./components/Skill";
+import Work from "./components/Work";
+import About from "./components/About";
 
 export default function Home() {
   return (
@@ -12,6 +15,9 @@ export default function Home() {
           <Sidebar/>
         </div>
         <div className="w-full" >
+          <About/>
+          <Work/>
+          <Skill/>
           <Contact/>
         </div>
       </div>
