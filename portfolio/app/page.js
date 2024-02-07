b75f06b76bc9f2ddde262f930b80d896aa868b96
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-02-07 00:15:00 -0300

    se creo component Contact-sin estilos-

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -2,15 +2,19 @@
 import Image from "next/image";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
     <main>
-    <div className="justify-center">
-
-    <Sidebar/>
-    </div>
-
+      <div className="flex" >
+        <div className="justify-center">
+          <Sidebar/>
+        </div>
+        <div className="w-full" >
+          <Contact/>
+        </div>
+      </div>
     </main>
   );
 }
